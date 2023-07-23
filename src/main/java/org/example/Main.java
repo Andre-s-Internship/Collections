c156bc022fc5b6435da85f91166a493dde31db27
icarus-77 <136583278+icarus-77@users.noreply.github.com>
2023-07-23 18:16:59 +0400

    Added tests for MyHashMap

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,12 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        HashMap<Integer, String> hashMap = new HashMap<>();
+        hashMap.put(1, "Java");
+        hashMap.put(2, "is");
+        hashMap.put(3, "fun");
+        hashMap.put(3, "Python");
+        System.out.println(hashMap.entrySet().getClass());
 
     }
 }
