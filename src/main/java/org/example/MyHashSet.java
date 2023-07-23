fdbbf9c5c5197fafd3c139f2060becd734640afe
icarus-77 <136583278+icarus-77@users.noreply.github.com>
2023-07-23 17:38:16 +0400

    Added MyHashSet implementation

--- src/main/java/org/example/MyHashSet.java
+++ src/main/java/org/example/MyHashSet.java
@@ -25,47 +25,82 @@ public class MyHashSet<E> implements Set<E> {
     }
 
     @Override
-    public boolean contains(Object o) {
-        return false;
+    public boolean contains(Object element) {
+        int hash = element.hashCode();
+        int index = Math.abs((hash) % CAPACITY);
+        MyLinkedList<E> target = hashtable.get(index);
+        if(target == null || target.isEmpty()) return false;
+        return target.contains(element);
     }
 
     @Override
-    public Iterator iterator() {
-        return null;
+    public Iterator<E> iterator() {
+        return new MySetIterator<>();
     }
 
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        MyLinkedList<Object> myLinkedList = new MyLinkedList<>();
+        for(int i = 0; i < CAPACITY; i++){
+            if(hashtable.get(i) == null) continue;
+            if(!hashtable.get(i).isEmpty()) {
+                myLinkedList.addAll(hashtable.get(i));
+            }
+        }
+
+        return myLinkedList.toArray();
     }
 
     @Override
-    public boolean add(Object o) {
-        if(contains(o)) return false;
-
-        int hash = o.hashCode();
+    public boolean add(E element) {
+        if(contains(element)) return false;
+        int hash = element.hashCode();
         int index = Math.abs((hash) % CAPACITY);
-        return false;
+        MyLinkedList<E> target;
+        if(hashtable.get(index) == null){
+            target = new MyLinkedList<>();
+            hashtable.add(index, target);
+        } else {
+            target = hashtable.get(index);
+        }
+        target.addLast(element);
+        size++;
+        return true;
     }
 
     @Override
-    public boolean remove(Object o) {
-        return false;
+    public boolean remove(Object element) {
+        if(!contains(element)) return false;
+        int hash = element.hashCode();
+        int index = (hash) % CAPACITY;
+        MyLinkedList<E> target = hashtable.get(index);
+        boolean removed = target.remove(element);
+        if (removed) {
+            size--;
+        }
+        return removed;
     }
 
     @Override
     public boolean addAll(Collection c) {
-        return false;
+        if(containsAll(c)) return false;
+        for(Object element : c) {
+            add((E) element);
+        }
+        return true;
     }
 
     @Override
     public void clear() {
-
+        hashtable.clear();
+        size = 0;
     }
 
-    @Override
     public boolean removeAll(Collection c) {
-        return false;
+        for(Object element : c) {
+            if(!remove(element)) return false;
+        }
+        return true;
     }
 
     @Override
@@ -75,11 +110,53 @@ public class MyHashSet<E> implements Set<E> {
 
     @Override
     public boolean containsAll(Collection c) {
-        return false;
+        for(Object element : c) {
+            if(!contains(element)) return false;
+        }
+        return true;
     }
 
     @Override
     public Object[] toArray(Object[] a) {
         return new Object[0];
+    }
+
+    boolean equalSets(Set<E> HashSet) {
+        return containsAll(HashSet) && HashSet.containsAll(this);
+    }
+
+
+    class MySetIterator<T> implements Iterator<E> {
+
+        MyLinkedList<E> list = new MyLinkedList<>();
+        Iterator<E> listIterator;
+
+        public MySetIterator(){
+            fillList();
+            listIterator = list.iterator();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return listIterator.hasNext();
+        }
+
+        @Override
+        public E next() {
+            return listIterator.next();
+        }
+
+        private void fillList(){
+            for(int i = 0; i < CAPACITY; i++){
+                if(hashtable.get(i) != null){
+                    if (!hashtable.get(i).isEmpty()) {
+                        Iterator<E> bucketList = (Iterator<E>) hashtable.get(i).iterator();
+                        while(bucketList.hasNext()){
+                            list.addFirst(bucketList.next());
+                        }
+                    }
+                }
+            }
+        }
     }
 }
